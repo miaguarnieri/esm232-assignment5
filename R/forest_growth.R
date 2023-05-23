c2de75e5f40195c1b8c361d6d837b583a6648d32
miag_bio <mia@timg.org>
2023-05-23 10:29:48 -0700

    starting forest growth model

---
+++ R/forest_growth.R
@@ -0,0 +1,18 @@
+#'  Forest growth model
+#' @param time time since start
+#' @param C forest carbon
+#' @param parms - as list with three values, r, K, closure
+#' @param r intrinsic growth rate (linear growth rate once canopy closure has been reached)
+#' @param K carrying capacity (kgC)
+#' @param g linear growth rate after closure
+#' @param closure canopy closure (kg C)
+#' @return derivative of population with time 
+
+dforestgrowth= function(Time, C, parms) {
+  
+  dC = ifelse(C < parms$closure, parms$r*C, parms$g*(1 - C/K))
+  
+  dC = ifelse(C >= parms$K, 0, dC)
+  
+  return(list(dC))
+}
